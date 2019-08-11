2f99970c1b42aca40a201a467c51cd174155de0a
anderson <anderson.reinkordt@gmail.com>
2019-08-10 20:01:32 -0500

    simplify csv parser sorting, remove csv type checking

--- app/nostra.rb
+++ app/nostra.rb
@@ -5,13 +5,8 @@ require_relative './nostra/constants.rb'
 require_relative './nostra/account.rb'
 
 module Nostra
-  def self.import_csv(csv_file, type = :one_money)
-    case type
-    when :one_money
+  def self.import_csv(csv_file)
       add_by_csv(csv_file)
-    else
-      raise "#{type} csv not implemented for import!"
-    end
   end
 
   private
@@ -24,26 +19,17 @@ module Nostra
   def self.add_transactions_by_account(transaction_rows)
     transactions_by_account = sort_by_from_account(transaction_rows)
 
-    transactions_by_account.each do |account|
-      new_account = Nostra::Account.new(account[:name])
-      new_account.add_transactions(account[:transactions])
+    transactions_by_account.each do |name, transactions|
+      new_account = Nostra::Account.new(name)
+      new_account.add_transactions(transactions)
     end
   end
 
   def self.sort_by_from_account(transaction_rows)
     sorted_transactions = transaction_rows.sort_by { |transaction| transaction[:from_account] }
 
-    collection = []
-    account_names = (sorted_transactions.each.collect { |transaction| transaction[:from_account] }).uniq
-
-    account_names.each { |account|
-      account_transactions = sorted_transactions.select { |transaction|
-        transaction[:from_account] == account
-      }
-      collection << {
-        name: account,
-        transactions: account_transactions
-      }
+    collection = sorted_transactions.group_by { |transaction|
+      transaction[:from_account]
     }
 
     collection
